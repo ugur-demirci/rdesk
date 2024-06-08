2e7fee66d78cd71b95a0d90f2dda30c1162636d7
Ugur <33366320+ugur-demirci@users.noreply.github.com>
2024-06-08 10:36:35 +0300

    Update rd.ps1

--- rd.ps1
+++ rd.ps1
@@ -1,1 +1,8 @@
 
+irm christitus.com/win | iex
+
+Get-ExecutionPolicy
+if Restricted
+Set-ExecutionPolicy AllSigned
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
